61ab054433bde2413f747068f0c439430b922784
Marco Barragan <mbarraga@thoughtworks.com>
2017-07-10 14:50:57 -0400

    Finished TODOs for 6-2

---
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,18 @@
+package android.example.com.visualizerpreferences;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
