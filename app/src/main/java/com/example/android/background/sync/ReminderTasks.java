532ec3b4a52831a18f493182fe0af0ef0224f190
Marco Barragan <mbarraga@thoughtworks.com>
2017-07-12 14:46:26 -0400

    Finished TODOs for 10-1

---
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -0,0 +1,20 @@
+package com.example.android.background.sync;
+
+import android.content.Context;
+
+import com.example.android.background.utilities.PreferenceUtilities;
+
+public class ReminderTasks {
+    public static String ACTION_INCREMENT_WATER_COUNT = "increment_count";
+
+    public static void executeTask(Context context, String action){
+        if (action.equals(ACTION_INCREMENT_WATER_COUNT)){
+            incrementWaterCount(context);
+        }
+    }
+
+    private static void incrementWaterCount(Context context){
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+
+}
